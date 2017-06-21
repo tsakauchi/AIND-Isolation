9c201f31aa61f6a016e6e22983543fd77e09a3cf
Tsuginosuke Sakauchi <dev@fairlandcircle.com>
2017-06-20 23:46:02 -0500

    feat: Add 3 custom heuristic functions

--- game_agent.py
+++ game_agent.py
@@ -3,7 +3,7 @@ test your agent's strength against a set of known agents using tournament.py
 and include the results in your report.
 """
 import random
-
+import isolation
 
 class SearchTimeout(Exception):
     """Subclass base exception for code clarity. """
@@ -34,8 +34,25 @@ def custom_score(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-    # TODO: finish this function!
-    raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    cur_move = game.get_player_location(player)
+    own_moves = game.get_legal_moves(player)
+    opp_moves = game.get_legal_moves(game.get_opponent(player))
+
+    nxt_move_score = len(own_moves) - len(opp_moves)
+
+    # if my current move is also reachable by the opponent,
+    # prefer that location over another location with the same number of own moves
+    opp_move_bonus = 0
+    if cur_move in opp_moves:
+        opp_move_bonus = 8
+
+    return nxt_move_score + opp_move_bonus
 
 
 def custom_score_2(game, player):
@@ -60,8 +77,25 @@ def custom_score_2(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-    # TODO: finish this function!
-    raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    cur_move = game.get_player_location(player)
+    own_moves = game.get_legal_moves(player)
+    opp_moves = game.get_legal_moves(game.get_opponent(player))
+
+    nxt_move_score = len(own_moves) - (len(opp_moves) * 2)
+
+    # if my current move is also reachable by the opponent,
+    # prefer that location over another location with the same number of own moves
+    opp_move_bonus = 0
+    if cur_move in opp_moves:
+        opp_move_bonus = 8
+
+    return nxt_move_score + opp_move_bonus # + cur_move_corner_bonus
 
 
 def custom_score_3(game, player):
@@ -86,8 +120,35 @@ def custom_score_3(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-    # TODO: finish this function!
-    raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    cur_move = game.get_player_location(player)
+    own_moves = game.get_legal_moves(player)
+    opp_moves = game.get_legal_moves(game.get_opponent(player))
+
+    nxt_move_score = len(own_moves) - len(opp_moves)
+
+    # prefer center of the board for the current move
+    cur_move_corner_bonus = corner_score(game, cur_move) * 8
+
+    return nxt_move_score + cur_move_corner_bonus
+
+
+# if current move is at the edge of the board, score = 0
+# if current move is 1 slot away from the edge of the board, score = 0.5
+# else score = 1
+def corner_score(game, move):
+    y, x = move
+    if x <= 0 or x >= game.width - 1 or y <= 0 or y >= game.height - 1:
+        return 0.0
+    elif x <= 1 or x >= game.width - 2 or y <= 1 or y >= game.height - 2:
+        return 0.5
+    else:
+        return 1.0
 
 
 class IsolationPlayer:
